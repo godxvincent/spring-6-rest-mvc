c07e6db1be01161183fde090ea37b86e2ec7ba00
godxvincent <godxvincent@gmail.com>
2025-05-26 08:35:22 -0600

    :sparkles: Adding header with path of created resource

--- src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
@@ -3,6 +3,7 @@ package guru.springframework.spring6restmvc.controllers;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,10 +30,14 @@ public class BeerController {
 
 
     @PostMapping()
-    public ResponseEntity createBeer(@RequestBody Beer beer){
+    public ResponseEntity<Beer> createBeer(@RequestBody Beer beer){
         Beer savedBeer = beerService.saveNewBeer(beer);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
         // return ResponseEntity.ok(savedBeer);
-        return new ResponseEntity(HttpStatus.CREATED);
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
 
